de0ee0ba4ec1217969dae3437b9f4a6229be9130
howardh <rubiks-cube@hotmail.com>
2011-07-08 02:34:00 -0400

    First commit!

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <curses.h>
+
+using namespace std;
+
+int main()
+{
+    char c;
+
+    initscr();
+    while (true)
+    {
+        switch (c = getch())
+        {
+            case 'a':
+                std::cout<<"Left"<<std::endl;
+                break;
+            case 's':
+                std::cout<<"Down"<<std::endl;
+                break;
+            case 'd':
+                std::cout<<"Right"<<std::endl;
+                break;
+            case 'w':
+                std::cout<<"Up"<<std::endl;
+                break;
+        }
+    }
+    endwin();
+    return 0;
+}
